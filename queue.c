fde899b9704f0e3b6abf18db41503727bb8ea06b
ChenFuHuang <kye1234321@gmail.com>
2024-02-24 23:44:55 +0800

    Implement q_new function
    with memory allocation and error handling

--- queue.c
+++ queue.c
@@ -14,7 +14,11 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *new_node = malloc(sizeof(struct list_head));
+    if (!new_node)
+        return NULL;
+    INIT_LIST_HEAD(new_node);
+    return new_node;
 }
 
 /* Free all storage used by queue */
