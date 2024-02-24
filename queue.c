60fff6bceffa3a99f64b83c87ff6c119c565b49d
ChenFuHuang <kye1234321@gmail.com>
2024-02-25 01:34:04 +0800

    Implement q_insert_head and q_insert_tail function

--- queue.c
+++ queue.c
@@ -39,12 +39,38 @@ void q_free(struct list_head *head)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *new_node = malloc(sizeof(element_t));
+    char *tmp = strdup(s);
+    if (!new_node || !tmp) {
+        free(new_node);
+        return false;
+    }
+
+    new_node->value = tmp;
+    list_add(&new_node->list, head);
+
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *new_node = malloc(sizeof(element_t));
+    char *tmp = strdup(s);
+    if (!new_node || !tmp) {
+        free(new_node);
+        return false;
+    }
+
+    new_node->value = tmp;
+    list_add_tail(&new_node->list, head);
+
     return true;
 }
 
