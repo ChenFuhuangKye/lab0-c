afc6a499528b2484f594085af38a637e6605ed03
ChenFuHuang <kye1234321@gmail.com>
2024-02-27 02:01:15 +0800

    Implemen q_sort function by quick_sort

--- queue.c
+++ queue.c
@@ -177,8 +177,35 @@ void q_reverseK(struct list_head *head, int k)
 }
 
 /* Sort elements of queue in ascending/descending order */
-void q_sort(struct list_head *head, bool descend) {}
+void quick_sort(struct list_head *head, struct list_head *end);
+void q_sort(struct list_head *head, bool descend)
+{
+    if (!head || list_empty(head))
+        return;
+    quick_sort(head, head);
+}
 
+void quick_sort(struct list_head *head, struct list_head *end)
+{
+    struct list_head *safe;
+    struct list_head *pivot = head->next;
+    char *value = list_entry(pivot, element_t, list)->value;
+    struct list_head *node = pivot->next;
+    while (node != end) {
+        element_t *elem = list_entry(node, element_t, list);
+        safe = node->next;
+        if (strcmp(elem->value, value) < 0) {
+            list_move_tail(node, pivot);
+        } else {
+            list_move(node, pivot);
+        }
+        node = safe;
+    }
+    if (head->next != pivot && head->next->next != pivot)
+        quick_sort(head, pivot);
+    if (pivot->next != end && pivot->next->next != end)
+        quick_sort(pivot, end);
+}
 /* Remove every node which has a node with a strictly less value anywhere to
  * the right side of it */
 int q_ascend(struct list_head *head)
